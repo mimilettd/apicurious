232cb999032180ac997a9b02cf70399eb7337e95
Dev Workstation <EMAIL@gmail.com>
2017-10-09 19:45:44 -0600

    user can sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "users#new"
+  root to: "home#index"
   get "/auth/github/callback" => "sessions#create"
-  get "/signout" => "sessions#destroy", :as => :signout
+  delete "/signout" => "sessions#destroy", :as => :signout
 end
